1edb44a4b3ed6ee9fcf1e1895cc70d87fbcd4522
Szymon Dąbrowski <szymonmiloszdabrowski@gmail.com>
2020-05-18 13:55:52 +0200

    dziala tryb dzien/noc, przycisk do zmiany avatara

--- Trening/app/src/main/java/com/example/planertreningow/MainActivity.java
+++ Trening/app/src/main/java/com/example/planertreningow/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.planertreningow;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -9,11 +11,18 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES){
+            setTheme(R.style.DarkTheme);
+        }
+        else{
+            setTheme(R.style.AppTheme);
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
     public void onClick(View view) {
-        setContentView(R.layout.ustawienia_konta);
+        Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+        startActivity(intent);
     }
 }
---
+++ Trening/app/src/main/java/com/example/planertreningow/SettingsActivity.java
@@ -0,0 +1,73 @@
+package com.example.planertreningow;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Switch;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+
+public class SettingsActivity extends AppCompatActivity {
+
+    private Switch nightMode;
+    private Button avatarButton;
+    private ImageView avatarImg;
+    private Integer counter = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES){
+            setTheme(R.style.DarkTheme);
+        }
+        else{
+            setTheme(R.style.AppTheme);
+        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+
+        avatarButton = findViewById(R.id.avatarButton);
+        avatarImg = findViewById(R.id.avatarImg);
+        nightMode = findViewById(R.id.tryb);
+
+        avatarButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View v) {
+                if(counter % 2 == 0){
+                    avatarImg.setImageDrawable(getDrawable(R.drawable.ic_smile));
+                }
+                else{
+                    avatarImg.setImageDrawable(getDrawable(R.drawable.ic_avatar));
+                }
+                counter++;
+            }
+        });
+        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES){
+            nightMode.setChecked(true);
+        }
+        nightMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    restartApp();
+                }
+                else{
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    restartApp();
+                }
+            }
+        });
+    }
+    public void restartApp(){
+        Intent i = new Intent(getApplicationContext(), SettingsActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
